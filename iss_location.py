2356ba485bf72baba71e78c851a99417cb4386ce
UTKARSH TRIPATHI <68503623+utkarshilh@users.noreply.github.com>
2025-11-22 01:07:24 +0530

    Add files via upload

---
+++ iss_location.py
@@ -0,0 +1,58 @@
+import requests 
+import json 
+import time 
+from  datetime import   datetime 
+
+import os 
+
+import csv 
+
+
+
+
+
+# creating output.csv file if not exits
+
+if not os.path.exists("output.csv"):
+    with open("output.csv","w", newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(["latitude","longitude","timestamp"])
+
+    print("file created successfully.") 
+
+else :
+    print("files alreaty exists")
+
+
+
+
+
+
+
+url = "http://api.open-notify.org/iss-now.json"
+
+# print("Tracking ISS location (press CTRL+C to stop)...")
+
+# while True :
+response = requests.get(url) 
+data = response.json()
+
+position = data["iss_position"]
+
+latitude = position["latitude"]
+
+longitude = position["longitude"]
+
+timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+with open("output.csv","a", newline= '') as f:
+    writer = csv.writer(f)
+    writer.writerow([latitude,longitude,timestamp])
+
+
+
+print(f"ISS Current Location -> Latitude : {latitude} , Longitude : {longitude} at {timestamp}")
+
+
+
+
